cbdd35a58b46bff50ad3749619e09fb8a4619567
Marcos Camargo <marcoscamargo@outlook.com>
2021-10-20 21:18:36 -0300

    Estruturas de Pastas

---
+++ Caelum-Java-Objetos-fj11/Capitulo3/Exercicio 3.3/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int gastosJan = 15000,gastosFev = 23000,gastosMar = 17000;    
+        int gastosTri = gastosJan + gastosFev + gastosMar;    
+        System.out.println(gastosTri);
+    }
+}
