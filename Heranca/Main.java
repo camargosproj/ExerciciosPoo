40f72d98550c76f8aa87cd403cebb5c0575f8b4e
Marcos Camargo <marcoscamargo@outlook.com>
2021-11-16 07:33:20 -0300

    Heranca 1 e 2

--- Heranca/Main.java
+++ Heranca/Main.java
@@ -1,44 +1,44 @@
 
 public class Main {
     public static void main(String[] args) {
-        Animal a1 = new Animal();
-        Animal a2 = new Mamifero();
-        Animal a3 = new Cachorro();
-        Animal a4 = new Morcego();
+        // Animal a1 = new Animal();
+        // Animal a2 = new Mamifero();
+        // Animal a3 = new Cachorro();
+        // Animal a4 = new Morcego();
 
-        Mamifero m1 = new Animal(); // Classe filha não pode instaciar classe pai
-        Mamifero m2 = new Mamifero();
-        Mamifero m3 = new Cachorro();
-        Mamifero m4 = new Morcego();
+        // Mamifero m1 = new Animal(); // Classe filha não pode instaciar classe pai
+        // Mamifero m2 = new Mamifero();
+        // Mamifero m3 = new Cachorro();
+        // Mamifero m4 = new Morcego();
 
-        Cachorro c1 = new Animal(); // Classe filha não pode instaciar classe pai
-        Cachorro c2 = new Mamifero(); // Classe filha não pode instaciar classe pai
-        Cachorro c3 = new Cachorro();
-        Cachorro c4 = new Morcego(); // Cachorro não tem relação com morcego pois não há herança
+        // Cachorro c1 = new Animal(); // Classe filha não pode instaciar classe pai
+        // Cachorro c2 = new Mamifero(); // Classe filha não pode instaciar classe pai
+        // Cachorro c3 = new Cachorro();
+        // Cachorro c4 = new Morcego(); // Cachorro não tem relação com morcego pois não há herança
 
-        Morcego mo1 = new Animal(); // Classe filha não pode instaciar classe pai
-        Morcego mo2 = new Mamifero(); // Classe filha não pode instaciar classe pai
-        Morcego mo3 = new Cachorro(); // Morcego não tem relação com cachorro pois não há herança
-        Morcego mo4 = new Morcego();
-        Animal a5 = new Cachorro();
-        a5.mover();
-        a5.mamar(); // Animal não possui metodo mamar, pois pertence a classe filha
-        Cachorro c5 = (Cachorro) a5;
-        c5.mover();
-        c5.mamar();
+        // Morcego mo1 = new Animal(); // Classe filha não pode instaciar classe pai
+        // Morcego mo2 = new Mamifero(); // Classe filha não pode instaciar classe pai
+        // Morcego mo3 = new Cachorro(); // Morcego não tem relação com cachorro pois não há herança
+        // Morcego mo4 = new Morcego();
+        // Animal a5 = new Cachorro();
+        // a5.mover();
+        // a5.mamar(); // Animal não possui metodo mamar, pois pertence a classe filha
+        // Cachorro c5 = (Cachorro) a5;
+        // c5.mover();
+        // c5.mamar();
 
-        Mamifero m5 = new Morcego();
-        m5.mover();
-        m5.mamar();
-        Morcego mo5 = (Morcego) m5;
-        mo5.mover();
-        mo5.mamar();
+        // Mamifero m5 = new Morcego();
+        // m5.mover();
+        // m5.mamar();
+        // Morcego mo5 = (Morcego) m5;
+        // mo5.mover();
+        // mo5.mamar();
 
-        Cachorro c6 = new Cachorro();
-        Mamifero m6 = c6;
-        Animal a6 = m6;
-        Morcego mo6 = a6; // Classe filha não pode instaciar classe pai
-        Verdadeiro ou Falso, se falso justifique.
+        // Cachorro c6 = new Cachorro();
+        // Mamifero m6 = c6;
+        // Animal a6 = m6;
+        // Morcego mo6 = a6; // Classe filha não pode instaciar classe pai
+        // Verdadeiro ou Falso, se falso justifique.
 
         // (Verdadeiro) Em orientação a objetos herança é a capacidade de uma classe definir o seu comportamento e sua estrutura 
         //    aproveitando definições de outra classe, normalmente conhecida como classe base ou classe pai.
